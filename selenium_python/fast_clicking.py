0765c0d48d09c0e905ebc51bfcbb363aec0c4269
Juan Piedra <juan.piedrarodriguez@windriver.com>
2021-07-22 09:53:38 -0600

    Agregando Chromedriver y dependencias

--- selenium_python/fast_clicking.py
+++ selenium_python/fast_clicking.py
@@ -1,7 +1,7 @@
 
 from selenium import webdriver
 import time
-DRIVER_LOCATION = './chromedriver'
+DRIVER_LOCATION = './../chromedriver'
 
 driver = webdriver.Chrome(DRIVER_LOCATION) #driver
 driver.get("https://clickspeedtest.com/5-seconds.html")
@@ -10,7 +10,9 @@ time.sleep(1) # pause before start
 print('Start clicking')
 while True: # click for ever
   try:
-    driver.find_element_by_css_selector('#clicker').click() 
+    #driver.find_element_by_css_selector('#clicker').click() 
+    #driver.find_element_by_id('clicker').click() 
+    driver.find_element_by_xpath("//button[@id='clicker']").click() 
   except Exception as ex: # until it breaks
     print('Time is over')
     break
@@ -21,7 +23,7 @@ print(f'Result: {result.text}\n')
 driver.close()
 
 
-# Result with ID Result: 199 Clicks in 5 Seconds
-# Result with CSS Result: 167 Clicks in 5 Seconds
-# Result with XPATH Result: 167 Clicks in 5 Seconds
+#css Result: 142 Clicks in 5 Seconds
+#id  Result: 147 Clicks in 5 Seconds
+
 
--- selenium_python/locators_strategy.py
+++ selenium_python/locators_strategy.py
@@ -1,13 +1,13 @@
 from selenium import webdriver
 import sys
-driver_location = './chromedriver'
+driver_location = './../chromedriver'
 
 driver = webdriver.Chrome(driver_location)
 driver.get('https://pjcalvo.github.io')
 
 try: 
     #combined parent child locator
-    parent_child_locator = '.post-list .post-links' 
+    parent_child_locator = '.post-list .post-link' 
     element = driver.find_element_by_css_selector(parent_child_locator)
 
     # #search the parent then search the child
@@ -16,7 +16,7 @@ try:
     # element = driver.find_element_by_css_selector(parent_locator) \
     #             .find_element_by_css_selector(child_locator) 
 
-    print(f'Element was found with href: { element.get_attribute("href")}')
+    print(f'Element was found with href: {element.get_attribute("href")}')
 except:
     print(sys.exc_info()[1])
 
