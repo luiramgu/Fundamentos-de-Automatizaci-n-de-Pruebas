5419a879bbc7b18282c94549a3bbcaa7ad4a9156
Juan Piedra <juan.piedrarodriguez@windriver.com>
2021-08-02 21:46:47 -0600

    Agregando archivos de la clase 5, semana 3

--- SeleniumFramework/src/test/java/TestAccount.java
+++ SeleniumFramework/src/test/java/TestAccount.java
@@ -1,22 +1,22 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import io.qameta.allure.Attachment;
+import io.qameta.allure.Description;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import java.sql.Time;
-import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
 
 public class TestAccount {
+
+    @Description("Validate test login was successful")
     @Test
     public void Test_Login_Successful(){
         String username = "juan.piedra@ucreativa.com";
         String password = "asdf";
 
-        String pathToDriver = Test.class.getResource("/chromedriver").getPath();
-        System.setProperty("webdriver.chrome.driver", pathToDriver);
+        //String pathToDriver = Test.class.getResource("/chromedriver").getPath();
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver");
 
         WebDriver driver = new ChromeDriver();
         driver.get("https://demo.opencart.com/");
@@ -38,9 +38,48 @@ public class TestAccount {
         driver.findElement(By.cssSelector("[value='Login']")).click();
 
         WebElement logOutButton = driver.findElement(By.linkText("Logout"));
-        Assert.assertTrue(!logOutButton.isDisplayed());
+        Assert.assertTrue(logOutButton.isDisplayed());
+
+        TakeScreenshot(driver);
 
         driver.close();
         driver.quit();
+    }
+
+    @Description("Validate that the login is working with non valid credentials")
+    @Test
+    public void Test_Login_Unsuccessful(){
+        String username = "juan.piedra@ucreativa.com";
+        String password = "asdfasdf";
+        String expectedMessage = "warning: no match for e-mail address and/or password.";
+
+        //String pathToDriver = Test.class.getResource("/chromedriver").getPath();
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver");
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://demo.opencart.com/");
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+
+        //Go To Login Page
+        driver.findElement(By.xpath("//*[@id=\"top-links\"]/ul/li[2]/a/span[1]")).click();
+        driver.findElement(By.linkText("Login")).click();
+
+        //Llenar formulario
+        driver.findElement(By.name("email")).sendKeys(username);
+        driver.findElement(By.name("password")).sendKeys(password);
+        driver.findElement(By.cssSelector("[value='Login']")).click();
+
+        WebElement alertMessage = driver.findElement(By.xpath("//div[contains(@class, 'alert-danger')]"));
+        Assert.assertEquals(expectedMessage.toLowerCase(), alertMessage.getText().toLowerCase().trim());
+
+        TakeScreenshot(driver);
+
+        driver.close();
+        driver.quit();
+    }
+
+    @Attachment(value = "screenshot", type = "image/png")
+    public byte[] TakeScreenshot(WebDriver driver){
+        return ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
     }
 }
